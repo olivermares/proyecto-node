b86525c025c6e3570d8388f70ca34c4726fed093
olivermares <olivermares@hotmail.com>
2023-07-25 12:38:02 +0200

    segunada

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const cors= require("cors")
 const {connect} = require("./src/utils/db")
 const comidaRoutes = require("./src/api/routes/comida.routes")
 
 const PORT = process.env.PORT;
 const app = express();
-connect()
+
+connect();
+app.use(cors({
+        origin:"*",
+        credential:true
+    }
+))
 
 app.use(express.json())
 
